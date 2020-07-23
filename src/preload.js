799160140e6859f6edbaa5438fbee70ef8504cba
Satoshi SAKAO <ottijp@users.noreply.github.com>
2020-07-24 02:51:17 +0900

    [fix] app can't be closed

--- src/preload.js
+++ src/preload.js
@@ -2,11 +2,6 @@ import { remote } from 'electron'
 import BuilderApp from './builder-app'
 import FileWatcher from './file-watcher'
 
-window.addEventListener('beforeunload', ev => {
-  // prevent reloading/closing
-  ev.returnValue = true
-})
-
 process.once('loaded', () => {
   window.electron = {
     dialog: remote.dialog,
