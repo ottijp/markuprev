b52561b39bff92df24afb038bb38d9480ca41d2a
Satoshi SAKAO <ottijp@users.noreply.github.com>
2020-07-31 22:48:25 +0900

    [refactor] load markdown css from .css

--- src/builtin-converter-md.js
+++ src/builtin-converter-md.js
@@ -1,5 +1,7 @@
 import checkbox from 'markdown-it-checkbox'
-import css from './github.css'
+import fs from 'fs'
+import path from 'path'
+import util from 'util'
 
 const md = require('markdown-it')({
   html: true,
@@ -8,7 +10,8 @@ const md = require('markdown-it')({
   .use(checkbox)
 
 export default class BuiltinConverterMd {
-  convert(source) {
+  async convert(source) {
+    const css = await util.promisify(fs.readFile)(path.join(__static, 'github.css'))
     const body = md.render(source)
     return `<!DOCTYPE html><html><head><meta charset="UFT-8"><style>${css}</style></head><body><div class="markdown-body">${body}</div></body></html>`
   }
